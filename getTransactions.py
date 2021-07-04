a5f8e77de397718e47a42631e38665e83f32106e
Ru <ruzhena.monastirska@gmail.com>
2021-07-04 15:09:29 +0300

    Actually added the headers to the request

--- getTransactions.py
+++ getTransactions.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 accountId = "5a59028c-e757-4f22-b88c-3ba90573383c"
-transactionsUrl = "https://api-sandbox.sebgroup.com/ais/v7/identified2/accounts/"+accountId+"/transactions"
+transactionsUrl = "https://api-sandbox.sebgroup.com/ais/v7/identified2/accounts/"+accountId+"/transactions?bookingStatus=both"
 
 with open("credentials.json") as credentialsFile:
     credentialsJson = json.load(credentialsFile)
@@ -10,10 +10,10 @@ with open("credentials.json") as credentialsFile:
 
 headers = {
     'Accept': "application/json",
-    'X-Request-ID': "randomstring",
+    'X-Request-ID': "WvqiIbLrevVN82r35UqgS9S1srsENuqI",
     'Authorization': "Bearer " + credentialsJson['bearer_token'],
     'PSU-Http-Method': "GET"
     }
 
-r = requests.get(transactionsUrl)
-print(r)
+r = requests.get(transactionsUrl, headers=headers)
+print(r.text)
